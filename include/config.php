ae1b1ea11333f5e2236623ebaaf64ebdc5ce30b8
tpm1983 <mail@jos16.de>
2013-05-30 22:18:25 +0200

    PATH points to dir if index.php is not in basedir.

--- include/config.php
+++ include/config.php
@@ -1,28 +1,28 @@
 <?php
 
-$_WS['GET'] = array_keys($_GET);
+$_WS['GET'] =           array_keys($_GET);
 
-$_WS['HASH']=			@$_WS['GET'][0];
-$_WS['HASH_REGEX']=		'/^[0-9a-z]{6}$/';
+$_WS['HASH']=           @$_WS['GET'][0];
+$_WS['HASH_REGEX']=     '/^[0-9a-z]{40}$/';
 
-$_WS['BASE_URI']=		'https://' . $_SERVER['SERVER_NAME'] . (($_SERVER['SERVER_PORT']!=80 && $_SERVER['SERVER_PORT']!=443) ? ':' . $_SERVER['SERVER_PORT'] : NULL) . '/';
+$_WS['BASE_URI']=       'https://' . $_SERVER['SERVER_NAME'] . (($_SERVER['SERVER_PORT']!=80 && $_SERVER['SERVER_PORT']!=443) ? ':' . $_SERVER['SERVER_PORT'] : NULL);
 
-$_WS['PATH']=			$_SERVER['DOCUMENT_ROOT'] . '/';
-$_WS['PATH_FILES']=		$_WS['PATH'] . 'files/';
+$_WS['PATH']=           dirname($_SERVER['SCRIPT_NAME']) . '/';
+$_WS['PATH_FILES']=     $_WS['PATH'] . 'files/';
 
-$_WS['EXCL_EXT']=		array('.exe');
-$_WS['EXCL_FILES']=		array('.', '..', '.DS_Store', '.Thumbs');
+$_WS['EXCL_EXT']=       array('.exe');
+$_WS['EXCL_FILES']=     array('.', '..', '.DS_Store', '.Thumbs');
 
-$_WS['SQL_CONNECT'][]=	@mysql_connect(
+$_WS['SQL_CONNECT'][]=  mysql_connect(
 
 	 // HOST
-	'sql.bembel.de',
+	'sql.server',
 
 	// USER
-	'root',
+	'USERNAME',
 
 	 // PASSWORD
-	'g37my31s!'
+	'PASS'
 );
 
 ?>
